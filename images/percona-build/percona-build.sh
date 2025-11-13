f86c31dc8580c05f6f870513de66c600b18d008e
春风化雨细如丝 <indiff@126.com>
2025-11-13 11:00:01 +0800

    Update percona-build.sh

--- images/percona-build/percona-build.sh
+++ images/percona-build/percona-build.sh
@@ -76,22 +76,22 @@ git clone --filter=blob:none --depth 1 https://github.com/cyrusimap/cyrus-sasl.g
 cd cyrus-sasl
 # sh autogen.sh
 
-export CFLAGS="-Wall "
-./autogen.sh --prefix="$DEPS_DST" \
-    --with-openssl="$DEPS_DST"
-    # --with-staticsasl
-env LDFLAGS="/opt/gcc-indiff/lib64:$DEPS_DST/lib:$DEPS_DST/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}" CC=/opt/gcc-indiff/bin/gcc CXX=/opt/gcc-indiff/bin/g++  \
-make install DESTDIR="$DEPS_DST" || true
+# export CFLAGS="-Wall "
+# ./autogen.sh --prefix="$DEPS_DST" \
+#     --with-openssl="$DEPS_DST"
+#     # --with-staticsasl
+# env LDFLAGS="/opt/gcc-indiff/lib64:$DEPS_DST/lib:$DEPS_DST/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}" CC=/opt/gcc-indiff/bin/gcc CXX=/opt/gcc-indiff/bin/g++  \
+# make install DESTDIR="$DEPS_DST" || true
+# # make -j$(nproc)
+# # make install
+
+# cd ..
+# git clone --filter=blob:none --depth 1 https://git.openldap.org/openldap/openldap.git
+# cd openldap
+# env CC=/opt/gcc-indiff/bin/gcc CXX=/opt/gcc-indiff/bin/g++ ./configure --prefix=$DEPS_DST --with-cyrus-sasl="$DEPS_DST" --with-tls="openssl"
+# env LDFLAGS="/opt/gcc-indiff/lib64:$DEPS_DST/lib:$DEPS_DST/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}" CC=/opt/gcc-indiff/bin/gcc CXX=/opt/gcc-indiff/bin/g++  \
 # make -j$(nproc)
 # make install
-
-cd ..
-git clone --filter=blob:none --depth 1 https://git.openldap.org/openldap/openldap.git
-cd openldap
-env CC=/opt/gcc-indiff/bin/gcc CXX=/opt/gcc-indiff/bin/g++ ./configure --prefix=$DEPS_DST --with-cyrus-sasl="$DEPS_DST" --with-tls="openssl"
-env LDFLAGS="/opt/gcc-indiff/lib64:$DEPS_DST/lib:$DEPS_DST/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}" CC=/opt/gcc-indiff/bin/gcc CXX=/opt/gcc-indiff/bin/g++  \
-make -j$(nproc)
-make install
 
 
 # build persona mysql
