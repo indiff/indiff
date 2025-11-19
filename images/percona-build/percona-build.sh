5ea7802bdf80d53a7e7ad39e8f721694b44e0eba
春风化雨细如丝 <indiff@126.com>
2025-11-19 10:17:54 +0800

    Fix environment variable export for build script

--- images/percona-build/percona-build.sh
+++ images/percona-build/percona-build.sh
@@ -77,11 +77,10 @@ cd cyrus-sasl
 # sh autogen.sh
 
 # export CFLAGS="-Wall "
-env LDFLAGS="/opt/gcc-indiff/lib64:$DEPS_DST/lib:$DEPS_DST/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}" CC=/opt/gcc-indiff/bin/gcc CXX=/opt/gcc-indiff/bin/g++  \
+env LDFLAGS="/opt/gcc-indiff/lib64:$DEPS_DST/lib:$DEPS_DST/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}" CC="/opt/gcc-indiff/bin/gcc" CXX="/opt/gcc-indiff/bin/g++" \
 ./autogen.sh --prefix="$DEPS_DST" \
     --with-openssl="$DEPS_DST"
     # --with-staticsasl
-
 # make install DESTDIR="$DEPS_DST" || true
 make -j$(nproc)
 make install
