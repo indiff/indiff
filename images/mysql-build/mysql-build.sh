ceaeed539e9fb66917705986c945cce9848274a6
春风化雨细如丝 <indiff@126.com>
2026-07-31 22:40:10 +0800

    更新 mysql-build.sh

--- images/mysql-build/mysql-build.sh
+++ images/mysql-build/mysql-build.sh
@@ -111,9 +111,14 @@ tar -xzf libtool-2.5.4.tar.gz
 cd libtool-2.5.4
 ./bootstrap  --force     # 如果存在
 ./configure --prefix=/usr
-make -j$(nproc)
-make install
+sudo make -j$(nproc)
+sudo make install
 cd ..
+# 3. 刷新 aclocal 缓存
+aclocal
+
+# 4. 重新生成 configure
+autoreconf -fvi
 
 # wget https://ftp.gnu.org/gnu/m4/m4-1.4.20.tar.gz
 wget_gnu m4/m4-1.4.20.tar.gz
