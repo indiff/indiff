30643a3efba62d1aaf1d2c9ac4fae6d2af1a711c
春风化雨细如丝 <indiff@126.com>
2026-08-02 19:21:57 +0800

    更新 fbmysql-build.sh

--- images/fbmysql-build/fbmysql-build.sh
+++ images/fbmysql-build/fbmysql-build.sh
@@ -194,7 +194,7 @@ unset PROTOC
 cmake .. -G Ninja \
     -DCMAKE_INSTALL_PREFIX=$DEPS_DST \
     -DCMAKE_C_FLAGS="-include stdint.h -include stddef.h -D__NO_STRING_INLINES -I$DEPS_DST/include -O2 -pipe -fPIC -DPIC -Wno-implicit-fallthrough -Wno-int-in-bool-context -Wno-shift-negative-value -Wno-misleading-indentation -Wno-format-overflow -Wno-nonnull -Wno-unused-function " \
-    -DCMAKE_CXX_FLAGS="-std=c++17 -include string -include memory -include cstdint -include cstddef -D__NO_STRING_INLINES -I$DEPS_DST/include -O2 -pipe -fPIC -DPIC -Wno-aligned-new -Wno-implicit-fallthrough -Wno-int-in-bool-context -Wno-shift-negative-value -Wno-misleading-indentation -Wno-format-overflow -Wno-nonnull -Wno-unused-function  " \
+    -DCMAKE_CXX_FLAGS="-std=c++17 -include string -include memory -include cstdint -include cstddef -Uin_range -D__NO_STRING_INLINES -I$DEPS_DST/include -O2 -pipe -fPIC -DPIC -Wno-aligned-new -Wno-implicit-fallthrough -Wno-int-in-bool-context -Wno-shift-negative-value -Wno-misleading-indentation -Wno-format-overflow -Wno-nonnull -Wno-unused-function  " \
     -DCMAKE_CXX_EXTENSIONS=OFF \
     -DENABLE_DTRACE=0 \
     -DCMAKE_EXE_LINKER_FLAGS="-L/opt/vcpkg/installed/x64-linux/lib -L/usr/lib64 -L$DEPS_DST/lib -L$DEPS_DST/lib64 $(pkg-config --static --libs protobuf) -Wl,--strip-all -Wl,--gc-sections -Wl,--no-as-needed -ldl" \
@@ -232,7 +232,7 @@ cmake .. -G Ninja \
     -DWITH_MECAB=OFF \
     -DWITH_EXT_BACKTRACE=OFF \
     -DWITH_NUMA=OFF \
-    -DWITH_PERFORMANCE_SCHEMA=ON \
+    -DWITH_PERFORMANCE_SCHEMA=ON -DHAVE_PSI_INTERFACE=1 \
     -DWITH_ARCHIVE_STORAGE_ENGINE=OFF \
     -DWITH_BLACKHOLE_STORAGE_ENGINE=OFF \
     -DWITH_EXAMPLE_STORAGE_ENGINE=ON \
