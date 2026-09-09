af2aea2f5ef767bcdab4ffeeda5e7c0e8a477ba5
春风化雨细如丝 <indiff@126.com>
2026-09-09 19:17:29 +0800

    更新 maxscale-build.sh

--- images/maxscale-build/maxscale-build.sh
+++ images/maxscale-build/maxscale-build.sh
@@ -128,7 +128,7 @@ cmake .. -G Ninja \
     -DNPM_EXECUTABLE=/opt/node-v26.8.1-linux-x64-glibc-217/bin/npm \
     -DBUILD_NOSQL=OFF \
     -DBUILD_TESTS=OFF \
-    -DFORCE_BUNDLE=OFF \
+    -DFORCE_BUNDLE=ON \
     -DWITH_SYSTEM_NODEJS=ON
 
 
